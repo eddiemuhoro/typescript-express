18e0feccb3750478cfd710345531db795166fab4
eddiemuhoro <eddiemuhoro@gmail.com>
2023-02-28 23:11:44 +0300

    added employees from database

--- auth/employer.js
+++ auth/employer.js
@@ -1,11 +1,49 @@
 import express from 'express';
-import dotenv from 'dotenv';
+
+import prisma from '../script.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Hello World...!');
+router.post('/register', async (req, res)=>{
+    const { name, email, password, phone } = req.body;
+    const employee = await prisma.employee.create({
+        data: {
+            name,
+            email,
+            password,
+            phone
+        },
+        select:{
+            id: true,
+            name: true,
+            email: true,
+        }
+    })
+    res.json(employee);
+}
+)
+
+
+router.post('/login', async (req, res)=>{
+    const { email, password } = req.body;
+    const employee = await prisma.employee.findUnique({
+        where: {
+            email: email
+        }
+    })
+    if(employee?.password === password){
+        res.json(employee);
+    }else{
+        res.json({message: 'Wrong credentials'});
     }
-);
+}
+)
+
+//get all employees
+router.get('/', async (req, res)=>{
+    const employees = await prisma.employee.findMany();
+    res.json(employees);
+}
+)
 
 export default router;
--- script.js
+++ script.js
@@ -4,5 +4,4 @@ const prisma = new PrismaClient();
 
 
 
-
 export default prisma
