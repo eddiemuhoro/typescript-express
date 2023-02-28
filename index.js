5983e4f58125376b7ddbd5994ed6ee178a592c16
eddiemuhoro <eddiemuhoro@gmail.com>
2023-02-28 22:03:54 +0300

    remove cors

--- index.js
+++ index.js
@@ -1,30 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import cors from 'cors';
 
 dotenv.config();
 
 
 const app = express();
-
-app.use(cors(
-    {
-        origin: '*',
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: ['Content-Type', 'Authorization']
-    }
-))
-
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    next();
-});
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 
 app.get('/', (req, res) => {
