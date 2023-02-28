74d4b89612e9272680469b0223714f13e7453822
eddiemuhoro <eddiemuhoro@gmail.com>
2023-02-28 21:40:18 +0300

    changed port

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+    }
+);
+
+
+const port= 4000 || process.env.PORT
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+}
+);
